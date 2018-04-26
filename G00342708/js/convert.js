abde44049fbfade18f0f0e7125b9d68e24e68260
declanmurphy7 <35809332+declanmurphy7@users.noreply.github.com>
2018-04-26 15:26:46 +0100

    Additional Functions

--- G00342708/js/convert.js
+++ G00342708/js/convert.js
@@ -8,3 +8,36 @@ function fToM(){
   document.getElementById("resultsentence").innerHTML = message;
 
 }
+
+function fToC(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.0348;
+  var message = measure + ' inches converts to ' + meters + ' meters.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+
+function fToY(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.3048;
+  var message = measure + ' yards converts to ' + meters + ' meters.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+
+function fToK(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.3048;
+  var message = measure + ' miles converts to ' + meters + ' meters.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
